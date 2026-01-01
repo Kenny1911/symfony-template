70a27c0e796a926f4588c42f946df601ecaa392b
Kenny1911 <o-muzyka@mail.ru>
2026-01-01 22:47:16 +0400

    Use php format for services and routes configuration

---
+++ config/services.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $di): void {
+    $di->import('../src/**/di.php');
+
+    if ('test' === $di->env()) {
+        $di->import('../tests/**/di.php');
+    }
+};
+
