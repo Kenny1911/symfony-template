049d650ddde16c6fa1aa242cee7fe7970c745408
Kenny1911 <o-muzyka@mail.ru>
2026-01-01 22:37:22 +0400

    Prepare and format code

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,11 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+/**
+ * @internal
+ * @psalm-internal App
+ */
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
