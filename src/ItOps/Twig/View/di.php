78dc49d708cdd74f644c17c38e29bc066b2d9d6f
Kenny1911 <o-muzyka@mail.ru>
2026-05-30 23:28:00 +0400

    Supports AttributeViewRenderer, fix di config

--- src/ItOps/Twig/View/di.php
+++ src/ItOps/Twig/View/di.php
@@ -4,21 +4,33 @@ declare(strict_types=1);
 
 namespace App\ItOps\Twig\View;
 
+use Kenny1911\TwigView\AttributeViewRenderer;
+use Kenny1911\TwigView\ChainRenderer;
 use Kenny1911\TwigView\Symfony\ViewListener;
 use Kenny1911\TwigView\ViewRenderer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\inline_service;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $di): void {
     $di->services()
-        ->set('twig.view_renderer', ViewRenderer::class)
-            ->args([
-                service('twig'),
-            ])
+        ->set('twig.view_renderer', ChainRenderer::class)
+        ->args([
+            [
+                inline_service(ViewRenderer::class)
+                    ->args([
+                        service('twig'),
+                    ]),
+                inline_service(AttributeViewRenderer::class)
+                    ->args([
+                        service('twig'),
+                    ]),
+            ],
+        ])
 
         ->set(ViewListener::class)
-            ->args([
-                service('twig.view_renderer'),
-            ])
-            ->tag('kernel.event_subscriber');
+        ->args([
+            service('twig.view_renderer'),
+        ])
+        ->tag('kernel.event_subscriber');
 };
